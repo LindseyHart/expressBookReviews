91932e2ccecaab2e7fa867e9e70feb2274904e8c
Lindsey <lu.ogrady@gmail.com>
2024-11-19 22:11:13 -0500

    updated login, reviewadded, deletereview endpoints

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,21 +9,49 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    //returns boolean
+    //write code to check if username and password match the one we have in records.
+    const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+    return matchingUsers.length > 0;
 }
-
-//only registered users can login
+  
+//  Only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({data:password}, "access", {expiresIn: 3600});
+      req.session.authorization = {accessToken,username};
+      return res.status(200).send("User successfully logged in");
+    }
+    else {
+      return res.status(208).json({message: "Invalid username or password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let user = req.user;
+  let review = req.body.review;
+  let book = books[isbn];
+
+  book.reviews[user] = review;
+  return res.status(200).json({ message: "Review successfully added." });
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Write your code here
+    let isbn = req.params.isbn;
+    let user = req.user;
+    let review = req.body.review;
+    let book = books[isbn];
+    delete book.reviews[user];
+  
+    return res.status(200).json({ message: "Deleted Sucessfully" });
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
